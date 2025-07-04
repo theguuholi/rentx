6f5c9c3bea995ac7cc86d4157973bde8888e323e
Gustavo <gustavo.oliveira@stord.com>
2025-07-04 09:17:36 -0300

    splash animation

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,39 +1,44 @@
-import { Button, Dimensions, StyleSheet } from 'react-native';
-import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import Animated, { Extrapolation, interpolate, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 import { Container } from './styles';
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
 import { useEffect } from 'react';
 
-const WIDTH = Dimensions.get('window').width;
-
 const Splash = () => {
-    const positionX = useSharedValue(0);
+    const splashAnimation = useSharedValue(0);
 
-    const positionStyle = useAnimatedStyle(() => {
+    const brandStyle = useAnimatedStyle(() => {
         return {
+            opacity: interpolate(splashAnimation.value, [0, 50], [1, 0]),
             transform: [{
-                translateX: positionX.value
-            }]
+                translateX: interpolate(splashAnimation.value, [0, 50], [0, -50], Extrapolation.CLAMP),
+            }],
         };
     });
 
-    const handleAnimationPosition = () => {
-        positionX.value = withTiming(Math.random() * (WIDTH - 100), { duration: 1000 });
-    };
+    const logoStyle = useAnimatedStyle(() => {
+        return {
+            opacity: interpolate(splashAnimation.value, [0, 25, 50], [0, 0.3, 1]),
+            transform: [{
+                translateX: interpolate(splashAnimation.value, [0, 50], [-50, 0], Extrapolation.CLAMP),
+            }],
+        };
+    });
+
+    useEffect(() => {
+        splashAnimation.value = withTiming(50, { duration: 1000 });
+    }, []);
 
     return (
         <Container>
-            <Animated.View style={[styles.container, positionStyle]} />
-            <Button title='Entrar' onPress={handleAnimationPosition} />
+            <Animated.View style={[brandStyle, { position: 'absolute' }]}>
+                <BrandSvg width={80} height={50} />
+            </Animated.View>
+            <Animated.View style={[logoStyle, { position: 'absolute' }]}>
+                <LogoSvg width={180} height={20} />
+            </Animated.View>
         </Container>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        width: 100,
-        height: 100,
-        backgroundColor: 'red',
-    },
-});
 
 export default Splash;
