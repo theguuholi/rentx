8730c659f06ab0aa347e6ecb675551ec1c7bd323
Gustavo <gustavo.oliveira@stord.com>
2025-07-09 06:49:06 -0300

    melhorar a experiencia do usuario

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Header, Subtitle, Title, Footer, Form } from './styles';
-import { StatusBar } from 'react-native';
+import { Keyboard, KeyboardAvoidingView, StatusBar, TouchableWithoutFeedback } from 'react-native';
 import Button from '../../components/Button';
 import { useTheme } from 'styled-components';
 import Input from '../../components/Input';
@@ -10,29 +10,33 @@ const Signin = () => {
   const theme = useTheme();
 
   return (
-    <Container>
-      <StatusBar
-        barStyle='dark-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      <Header>
-        <Title>Estamos{'\n'}quase lá.</Title>
-        <Subtitle>
-          Faça seu login para começar{'\n'}
-          uma experiência incrível.
-        </Subtitle>
-      </Header>
-      <Form>
-        <Input iconName='mail' placeholder='E-mail' autoCorrect={false} autoCapitalize='none' keyboardType='email-address' />
-        <PasswordInput iconName='lock' placeholder='Senha' autoCorrect={false} autoCapitalize='none' />
-      </Form>
+    <KeyboardAvoidingView behavior='height' enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar
+            barStyle='dark-content'
+            backgroundColor='transparent'
+            translucent
+          />
+          <Header>
+            <Title>Estamos{'\n'}quase lá.</Title>
+            <Subtitle>
+              Faça seu login para começar{'\n'}
+              uma experiência incrível.
+            </Subtitle>
+          </Header>
+          <Form>
+            <Input iconName='mail' placeholder='E-mail' autoCorrect={false} autoCapitalize='none' keyboardType='email-address' />
+            <PasswordInput iconName='lock' placeholder='Senha' autoCorrect={false} autoCapitalize='none' />
+          </Form>
 
-      <Footer>
-        <Button title='Login' onPress={() => { }} />
-        <Button title='Criar conta gratuita' light={true} color={theme.colors.background_secondary} onPress={() => { }} />
-      </Footer>
-    </Container>
+          <Footer>
+            <Button title='Login' onPress={() => { }} />
+            <Button title='Criar conta gratuita' light={true} color={theme.colors.background_secondary} onPress={() => { }} />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
