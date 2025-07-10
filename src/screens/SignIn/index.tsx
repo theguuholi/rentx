a1ddcf4dacb35ed803a191db7ef9c9b77853665a
Gustavo <gustavo.oliveira@stord.com>
2025-07-10 09:23:10 -0300

    faz login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,18 +1,45 @@
 import React, { useState } from 'react';
 import { Container, Header, Subtitle, Title, Footer, Form } from './styles';
-import { Keyboard, KeyboardAvoidingView, StatusBar, TouchableWithoutFeedback } from 'react-native';
+import {
+  Alert,
+  Keyboard,
+  KeyboardAvoidingView,
+  StatusBar,
+  TouchableWithoutFeedback,
+} from 'react-native';
 import Button from '../../components/Button';
 import { useTheme } from 'styled-components';
 import Input from '../../components/Input';
 import PasswordInput from '../../components/PasswordInput';
+import * as Yup from 'yup';
 
 const Signin = () => {
   const theme = useTheme();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSignIn = () => {
-    console.log(email, password);
+  const handleSignIn = async () => {
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+
+      await schema.validate({ email, password });
+
+      Alert.alert('Tudo certo', 'Você está logado!');
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert('Opa', error.message);
+      } else {
+        Alert.alert(
+          'Erro na autenticação',
+          'Ocorreu um erro ao fazer login, verifique as credenciais.'
+        );
+      }
+    }
   };
 
   return (
@@ -32,25 +59,33 @@ const Signin = () => {
             </Subtitle>
           </Header>
           <Form>
-            <Input iconName='mail'
+            <Input
+              iconName='mail'
               placeholder='E-mail'
               autoCorrect={false}
               autoCapitalize='none'
               keyboardType='email-address'
               value={email}
-              onChangeText={setEmail} />
+              onChangeText={setEmail}
+            />
             <PasswordInput
               iconName='lock'
               placeholder='Senha'
               autoCorrect={false}
               autoCapitalize='none'
               value={password}
-              onChangeText={setPassword} />
+              onChangeText={setPassword}
+            />
           </Form>
 
           <Footer>
             <Button title='Login' onPress={handleSignIn} />
-            <Button title='Criar conta gratuita' light={true} color={theme.colors.background_secondary} onPress={() => { }} />
+            <Button
+              title='Criar conta gratuita'
+              light={true}
+              color={theme.colors.background_secondary}
+              onPress={() => {}}
+            />
           </Footer>
         </Container>
       </TouchableWithoutFeedback>
